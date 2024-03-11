a7482d68e359809dea8729849d8fbbf640fc9339
WilfriedW9 <zalo.Timothe@gmail.com>
2024-03-11 16:08:46 +0100

    Implémentation nombre de cycles

--- script.js
+++ script.js
@@ -2,29 +2,27 @@ const buttons = document.querySelectorAll("button");
 const hoursDisplay = document.querySelector("#hours");
 const minutesDisplay = document.querySelector("#minutes");
 const secondsDisplay = document.querySelector("#seconds");
-console.log(minutesDisplay, secondsDisplay);
+const cycleCounter = document.querySelector("#cycleCounter")
 let hours = 0;
 let minutes = 25;
 let seconds = 0;
+let cycle = 0
+
+cycleCounter.innerText = cycle
 
 hoursDisplay.innerHTML = hours;
 minutesDisplay.innerHTML = minutes;
 secondsDisplay.innerHTML = hours;
 
-function hello() {
-  console.log("hello there ! ");
-}
-
 buttons.forEach((button) => {
-  console.log(button.value);
   hoursDisplay.innerHTML = hours;
   button.addEventListener("click", () => {
     switch (button.value) {
       case "addHours":
         hours += 1;
         if (hours > 99) {
-            hours = 0;
-          }
+          hours = 0;
+        }
         hoursDisplay.innerHTML = hours;
         break;
       case "substractHours":
@@ -37,8 +35,8 @@ buttons.forEach((button) => {
       case "addMinutes":
         minutes += 1;
         if (minutes > 59) {
-            minutes = 0;
-          }
+          minutes = 0;
+        }
         minutesDisplay.innerHTML = minutes;
         break;
       case "substractMinutes":
@@ -52,8 +50,8 @@ buttons.forEach((button) => {
       case "addSeconds":
         seconds += 1;
         if (seconds > 59) {
-            seconds = 0;
-          }
+          seconds = 0;
+        }
         secondsDisplay.innerHTML = seconds;
         break;
       case "substractSeconds":
@@ -63,12 +61,13 @@ buttons.forEach((button) => {
         }
         secondsDisplay.innerHTML = seconds;
         break;
+      case "start":
+        button.innerText === "Start Timer" ? button.innerText = "Stop !" : button.innerText = "Start Timer"
+        button.classList.contains("start") ? button.classList.remove("start") : button.classList.add("start")
+        break;
     }
   });
 
-  button.addEventListener("mousedown", () => {
-  });
-  button.addEventListener("mouseup", () => {
-  });
+  button.addEventListener("mousedown", () => {});
+  button.addEventListener("mouseup", () => {});
 });
-
